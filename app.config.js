002a55043d351fed07442ab8bfc3b56de96afb77
FTCHD <144691102+FTCHD@users.noreply.github.com>
2025-12-13 17:53:40 +0700

    we commit (again)
    Signed-off-by: FTCHD <144691102+FTCHD@users.noreply.github.com>

---
+++ app.config.js
@@ -0,0 +1,93 @@
+//  https://docs.expo.dev/workflow/configuration/#switching-configuration-based-on-the-environment
+//  https://docs.expo.dev/versions/latest/config/app/#backgroundcolor
+//  https://docs.expo.dev/versions/latest/config/app/#primarycolor
+
+module.exports = ({ config }) => {
+    return {
+        ...config,
+        primaryColor: '#13111c',
+        backgroundColor: '#13111c',
+
+        name: process.env.EXPO_PUBLIC_APP_NAME,
+        slug: process.env.EXPO_PUBLIC_APP_SLUG,
+        scheme: process.env.EXPO_PUBLIC_APP_SCHEME,
+        version: process.env.EXPO_PUBLIC_APP_VERSION,
+        owner: process.env.EXPO_PUBLIC_OWNER,
+
+        orientation: 'portrait',
+        icon: './assets/icon.png',
+        userInterfaceStyle: 'dark',
+        newArchEnabled: true,
+
+        ios: {
+            appleTeamId: process.env.EXPO_PUBLIC_APPLE_TEAM_ID,
+            bundleIdentifier: process.env.EXPO_PUBLIC_IOS_BUNDLE_IDENTIFIER,
+            supportsTablet: true,
+            config: {
+                usesNonExemptEncryption: false,
+            },
+            infoPlist: {
+                SKIncludeConsumableInAppPurchaseHistory: true,
+            },
+        },
+
+        androidNavigationBar: {
+            enforceContrast: false,
+        },
+        android: {
+            package: process.env.EXPO_PUBLIC_ANDROID_PACKAGE,
+            adaptiveIcon: {
+                foregroundImage: './assets/icon.png',
+                backgroundColor: '#13111c',
+            },
+            googleServicesFile: './google-services.json',
+            playStoreUrl: process.env.EXPO_PUBLIC_ANDROID_STORE_URL,
+            predictiveBackGestureEnabled: false,
+        },
+
+        plugins: [
+            [
+                'expo-build-properties',
+                {
+                    android: {
+                        minSdkVersion: 26,
+                    },
+                },
+            ],
+            './plugins/withAndroidHeap',
+            'expo-router',
+
+            [
+                'expo-splash-screen',
+                {
+                    image: './assets/icon.png',
+                    resizeMode: 'contain',
+                    backgroundColor: '#13111c',
+                    imageWidth: 200,
+                },
+            ],
+            'expo-quick-actions',
+            [
+                '@sentry/react-native/expo',
+                {
+                    url: 'https://sentry.io/',
+                    project: process.env.EXPO_PUBLIC_SENTRY_PROJECT,
+                    organization: process.env.EXPO_PUBLIC_SENTRY_ORG,
+                },
+            ],
+            'expo-web-browser',
+        ],
+
+        experiments: {
+            typedRoutes: true,
+        },
+        extra: {
+            router: {
+                origin: false,
+            },
+            eas: {
+                projectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID,
+            },
+        },
+    }
+}
